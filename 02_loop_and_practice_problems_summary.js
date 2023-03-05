d279e4b2f65f35fda44d86dda8a737d341247acf
developer-abdulaziz <abdulazizwebdbdeveloper1996@gmail.com>
2023-03-05 22:41:49 +0600

    loop add

--- 02_loop_and_practice_problems_summary.js
+++ 02_loop_and_practice_problems_summary.js
@@ -18,3 +18,72 @@
 //     console.log(i);
 // }
 
+
+
+/*================ while loop ===============*/
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log('roast dan plz');
+//     roastGiven++
+//     console.log(roastGiven);
+// }
+
+
+// var num = 1;
+// while(num<= 20){
+//     console.log(num);
+//     num+=2;
+// }
+
+// 1-10
+// for(var i = 0; i <=10; i++){
+//     console.log(i);
+// }
+
+
+/*
+// even number 1-10
+for(var i = 0; i <=10; i+=2){
+        console.log(i);
+    }
+ */
+
+
+/*
+// odd number 1-10
+for(var i = 1; i <=10; i+=2){
+    console.log(i);
+}
+*/
+
+
+/*================ array ==============*/
+/*
+var items = ['apple', 'banana', 'fruits', 'orange', 'anaros'];
+items [2] = 'kola';
+items.push('redy');
+items.pop();
+console.log(items);
+
+console.log(items.indexOf('banana'));
+console.log(items[2]);
+var reversed = items.reverse();
+console.log(reversed);
+
+
+/*
+var items = ['apple', 'banana', 'fruits', 'orange'];
+for (var i = 0; i < items.length; i++){
+    var element = items[i];
+    console.log(element);
+}
+*/
+
+// prblm solving
+// var fruits = ['apple', 'banana', 'orange'];
+// // var bananaIndex = fruits.indexOf('banana');
+// // console.log(bananaIndex);
+// fruits[1] = 'mango';
+// fruits.pop()
+// fruits.push('watermelon')
+// console.log(fruits);
